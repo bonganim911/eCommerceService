6efd45ea2d44122a38e0cc36b503e821cd1da624
Bongani <bonganim911@@gmail.com>
2022-07-13 17:49:54 +0200

    Added another test to cater for list with random inventories with discounts and extra quantity.

--- src/test/java/com/bongani/ECommerce/service/ECommerceServiceTest.java
+++ src/test/java/com/bongani/ECommerce/service/ECommerceServiceTest.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 public class ECommerceServiceTest {
@@ -119,6 +120,59 @@ public class ECommerceServiceTest {
 
         when(inventoryRepository.findById(any())).thenReturn(databaseResponse);
         when(quantityDiscountRepository.findByInventoryId(any())).thenReturn(quantityDiscountResponse);
+
+        TotalCostResponse price = eCommerceService.getPrice(inventoryLists);
+
+        assertEquals(expectedTotalCosts, price.getPrice());
+
+    }
+
+    @Test
+    void shouldReturnPriceGivenInventoryIdWithDiscountWhenExtraQuantityWithMultipleInventories() {
+        Inventory inventory = new Inventory();
+        List<String> inventoryLists = new ArrayList<>();
+        inventory.setPrice(BigDecimal.valueOf(100));
+
+        Inventory secondInventory = new Inventory();
+        secondInventory.setPrice(BigDecimal.valueOf(80));
+
+        Inventory thirdInventory = new Inventory();
+        thirdInventory.setPrice(BigDecimal.valueOf(50));
+
+        inventoryLists.add("001");
+        inventoryLists.add("002");
+        inventoryLists.add("003");
+        inventoryLists.add("001");
+        inventoryLists.add("001");
+        inventoryLists.add("002");
+        inventoryLists.add("002");
+        inventoryLists.add("001");
+
+        QuantityDiscount quantityDiscountForFirstInventory = new QuantityDiscount();
+        quantityDiscountForFirstInventory.setQuantity(3);
+        quantityDiscountForFirstInventory.setPrice(BigDecimal.valueOf(200));
+
+        QuantityDiscount quantityDiscountForSecondInventory = new QuantityDiscount();
+        quantityDiscountForSecondInventory.setQuantity(2);
+        quantityDiscountForSecondInventory.setPrice(BigDecimal.valueOf(120));
+
+        BigDecimal expectedTotalCosts = BigDecimal.valueOf(550);
+
+        Optional<Inventory> databaseResponse = Optional.of(inventory);
+        Optional<Inventory> secondDatabaseResponse = Optional.of(secondInventory);
+        Optional<Inventory> thirdDatabaseResponse = Optional.of(thirdInventory);
+
+        Optional<QuantityDiscount> quantityDiscountResponseForFirstInventory = Optional.of(quantityDiscountForFirstInventory);
+        Optional<QuantityDiscount> quantityDiscountResponseForSecondInventory = Optional.of(quantityDiscountForFirstInventory);
+        Optional<QuantityDiscount> quantityDiscountResponseForThirdInventory = Optional.empty();
+
+        when(inventoryRepository.findById(eq(Long.valueOf("001")))).thenReturn(databaseResponse);
+        when(inventoryRepository.findById(eq(Long.valueOf("002")))).thenReturn(secondDatabaseResponse);
+        when(inventoryRepository.findById(eq(Long.valueOf("003")))).thenReturn(thirdDatabaseResponse);
+
+        when(quantityDiscountRepository.findByInventoryId(eq(Integer.parseInt("001")))).thenReturn(quantityDiscountResponseForFirstInventory);
+        when(quantityDiscountRepository.findByInventoryId(eq(Integer.parseInt("002")))).thenReturn(quantityDiscountResponseForSecondInventory);
+        when(quantityDiscountRepository.findByInventoryId(eq(Integer.parseInt("003")))).thenReturn(quantityDiscountResponseForThirdInventory);
 
         TotalCostResponse price = eCommerceService.getPrice(inventoryLists);
 
