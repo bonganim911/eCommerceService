cb8a340d1d4393758592f1b18f05b4e3b650ea11
Bongani <bonganim911@@gmail.com>
2022-07-13 20:31:26 +0200

    Added the H2 in=memory database and init data script to populate the data in inventory and discount tables.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Inventory (id, name, price) VALUES (001, 'Rolex', 100);
+INSERT INTO Inventory (id, name, price) VALUES (002, 'Michael Kors', 80);
+INSERT INTO Inventory (id, name, price) VALUES (003, 'Swatch', 50);
+INSERT INTO Inventory (id, name, price) VALUES (004, 'Casio', 30);
+
+INSERT INTO Quantity_Discount (id, inventory_id, price, quantity) VALUES (1, 001, 200, 3);
+INSERT INTO Quantity_Discount (id, inventory_id, price, quantity) VALUES (2, 002, 120, 2);
