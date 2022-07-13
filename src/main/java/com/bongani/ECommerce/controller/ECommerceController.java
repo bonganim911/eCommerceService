9eb83e469273dc5cd2da1c00066fb973dc12d5f1
Bongani <bonganim911@@gmail.com>
2022-07-13 16:34:05 +0200

    Created controller with test expectation to pass the test along with concrete controller.

--- src/main/java/com/bongani/ECommerce/controller/ECommerceController.java
+++ src/main/java/com/bongani/ECommerce/controller/ECommerceController.java
@@ -1,4 +1,24 @@
 package com.bongani.ECommerce.controller;
 
+import com.bongani.ECommerce.dtos.TotalCostResponse;
+import com.bongani.ECommerce.service.ECommerceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ECommerceController {
+
+    @Autowired
+    private ECommerceService eCommerceService;
+
+    @GetMapping("/checkout")
+    public ResponseEntity<TotalCostResponse> getTotalPrice(@RequestBody List<String> inventories){
+        return new ResponseEntity<>(eCommerceService.getPrice(inventories), HttpStatus.OK);
+    }
 }
