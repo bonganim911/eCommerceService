646fe940dc86b44489e387e5ab92de7982435ad8
Bongani <bonganim911@@gmail.com>
2022-07-13 16:00:41 +0200

    Created init springboot project with required dependencies.

---
+++ src/test/java/com/bongani/ECommerceService/ECommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bongani.ECommerceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
